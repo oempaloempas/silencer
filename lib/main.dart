a28eaab1a4519f3162b183d04da22a9f24ce0ff9
Stijn Vroegindeweij <stijnvidw1@gmail.com>
2023-10-19 10:50:57 +0200

    Add no-trace logger option

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:logger/logger.dart';
 import 'package:silencer/services/auth.dart';
+import 'package:silencer/services/logging.dart';
 import 'package:silencer/services/navigation.dart';
 import 'firebase_options.dart';
 
@@ -9,6 +12,12 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initFirebase();
+  if (kReleaseMode) {
+    Logging.changeLogLevel(Level.error);
+  } else {
+    Logging.changeLogLevel(Level.all);
+  }
+  Logging.infoLog('If you see this message, this is a debug build');
   runApp(const MyApp());
 }
 
--- lib/services/logging.dart
+++ lib/services/logging.dart
@@ -1,26 +1,50 @@
 import 'package:logger/logger.dart';
 
 class Logging {
-  static final _logger = Logger();
+  static final Logger _logger = Logger(printer: PrettyPrinter());
+  static final Logger _noTraceLogger = Logger(
+      printer: PrettyPrinter(
+    methodCount: 0,
+  ));
 
-  static void traceLog(String traceMessage) {
-    _logger.t(traceMessage);
+  static void traceLog(String traceMessage, {bool trace = true}) {
+    if (trace) {
+      _logger.t(traceMessage);
+    } else {
+      _noTraceLogger.t(traceMessage);
+    }
   }
 
-  static void debugLog(String debugMessage) {
-    _logger.d(debugMessage);
+  static void debugLog(String debugMessage, {bool trace = true}) {
+    if (trace) {
+      _logger.d(debugMessage);
+    } else {
+      _noTraceLogger.d(debugMessage);
+    }
   }
 
-  static void infoLog(String infoMessage) {
-    _logger.i(infoMessage);
+  static void infoLog(String infoMessage, {bool trace = true}) {
+    if (trace) {
+      _logger.i(infoMessage);
+    } else {
+      _noTraceLogger.i(infoMessage);
+    }
   }
 
-  static void warningLog(String warningMessage) {
-    _logger.w(warningMessage);
+  static void warningLog(String warningMessage, {bool trace = true}) {
+    if (trace) {
+      _logger.w(warningMessage);
+    } else {
+      _noTraceLogger.w(warningMessage);
+    }
   }
 
-  static void errorLog(String errorMessage, Object errorObject) {
-    _logger.e(errorMessage);
+  static void errorLog(String errorMessage, {bool trace = true}) {
+    if (trace) {
+      _logger.e(errorMessage);
+    } else {
+      _noTraceLogger.e(errorMessage);
+    }
   }
 
   static void changeLogLevel(Level loglevel) {

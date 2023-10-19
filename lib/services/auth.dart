2fda0228c8a264459c09c83151ecbc973fcbb2eb
Stijn Vroegindeweij <stijnvidw1@gmail.com>
2023-10-19 16:41:13 +0200

    Added untested google sign-in

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Auth {
   // Instance
@@ -51,10 +53,39 @@ class Auth {
     }
   }
 
-  Future<void> signInWithGoogle(
-      {required String email,
-      required String password,
-      Function? whenDone}) async {}
+  Future<UserCredential> signInWithGoogle() async {
+    // Login when requested from a mobile device
+    Future<UserCredential> signInMobile() async {
+      // Letting the user log in
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Return the account info
+      return await _firebaseAuth.signInWithCredential(credential);
+    }
+
+    // Login when requested from a web device
+    Future<UserCredential> signInWeb() async {
+      GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+      return await _firebaseAuth.signInWithPopup(googleProvider);
+    }
+
+    if (kIsWeb) {
+      return signInWeb();
+    } else {
+      return signInMobile();
+    }
+  }
 
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
